4f772e1edff1b98414459ac363ea2be0b3cda689
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-02-08 22:31:51 +0500

    Update q2.cpp

--- Lab 3/q2.cpp
+++ Lab 3/q2.cpp
@@ -1,3 +1,7 @@
+/*Create a class called Date that includes three pieces of information as instance variables—a month (typeint), a day
+(typeint) and a year (typeint). Provide a method displayDate that displays the month, day and year separated by
+forward slashes(/). Write a test application named DateTest that demonstrates classDate’s capabilities.*/
+
 #include<iostream>
 using namespace std;
 
